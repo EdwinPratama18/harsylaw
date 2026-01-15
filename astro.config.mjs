1447d04daf171e71576e8356007cec2b9a3a1daa
EdwinPratama18 <edwinpratama130@gmail.com>
2026-01-16 00:59:31 +0700

    add SEO: sitemap, meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://harsylaw.github.io',
+  integrations: [tailwind(), sitemap()],
+  site: 'https://harsylaw.vercel.app',
 });
